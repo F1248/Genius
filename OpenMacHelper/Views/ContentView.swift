298bef8ba37b130b1c36dbce094a8a9529aa5bd3
F1248 <f1248@mailbox.org>
2024-03-15 18:09:36 +0100

    Remove unused

--- OpenMacHelper/Views/ContentView.swift
+++ OpenMacHelper/Views/ContentView.swift
@@ -9,9 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @AppStorage("userMode")
-    var userMode: UserMode = .averageUser
-    
     var body: some View {
         NavigationView {
             List {
