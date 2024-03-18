382a2b8a15f769a64598037f95b250e8446484e7
F1248 <f1248@mailbox.org>
2024-03-18 16:33:48 +0100

    Replace NavigationView with NavigationSplitView on macOS >= 13.0

--- OpenMacHelper/Views/ContentView.swift
+++ OpenMacHelper/Views/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        NavigationView {
-            SidebarView()
+        if #available(macOS 13.0, *) {
+            NavigationSplitView {
+                SidebarView()
+            } detail: {
+                EmptyView()
+            }
+            .frame(minWidth: 512, minHeight: 256)
+        } else {
+            NavigationView {
+                SidebarView()
+            }
+            .frame(minWidth: 512, minHeight: 256)
         }
-        .frame(minWidth: 512, minHeight: 256)
     }
 }
 
