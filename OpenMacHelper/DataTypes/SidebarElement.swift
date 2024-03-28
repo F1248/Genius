904d491868d36efeb40d0a8f7378f632e10fa942
F1248 <f1248@mailbox.org>
2024-03-28 13:06:46 +0100

    Optimise selection in Sidebar, add to each tag

---
+++ OpenMacHelper/DataTypes/SidebarElement.swift
@@ -0,0 +1,13 @@
+//
+//  SidebarElements.swift
+//  OpenMacHelper
+//
+//  Created by F1248.
+//
+
+enum SidebarElement {
+    case home
+    case systemInformation
+    case maintenance
+    case settings
+}
--- OpenMacHelper/Views/SidebarView.swift
+++ OpenMacHelper/Views/SidebarView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct SidebarView: View {
 
-    @State private var selectedView: String? = "Home"
+    @State private var selectedSidebarElement: SidebarElement? = .home
 
     @AppStorage("interfaceMode")
     var interfaceMode: InterfaceMode = .normal
 
     var body: some View {
-        List(selection: $selectedView) {
+        List(selection: $selectedSidebarElement) {
             NavigationLink {
                 HomeView()
             } label: {
@@ -23,7 +23,7 @@ struct SidebarView: View {
                     .font(.title2)
                     .padding(4)
             }
-            .tag("Home")
+            .tag(SidebarElement.home)
             .keyboardShortcut("0", modifiers: [.command])
             NavigationLink {
                 SystemInformationView()
@@ -32,6 +32,7 @@ struct SidebarView: View {
                     .font(.title2)
                     .padding(4)
             }
+            .tag(SidebarElement.systemInformation)
             .keyboardShortcut("1", modifiers: [.command])
             NavigationLink {
                 MaintenanceView()
@@ -40,6 +41,7 @@ struct SidebarView: View {
                     .font(.title2)
                     .padding(4)
             }
+            .tag(SidebarElement.maintenance)
             .keyboardShortcut("2", modifiers: [.command])
             NavigationLink {
                 SettingsView()
@@ -48,6 +50,7 @@ struct SidebarView: View {
                     .font(.title2)
                     .padding(4)
             }
+            .tag(SidebarElement.settings)
             .keyboardShortcut(",", modifiers: [.command])
         }
         .frame(minWidth: 256)
