105ed844e468ce86e68064de8c2406198e37c519
F1248 <f1248@mailbox.org>
2024-04-04 16:31:48 +0200

    Improve code style: group variables and functions

--- OpenMacHelper/DataTypes/Settings.swift
+++ OpenMacHelper/DataTypes/Settings.swift
@@ -15,10 +15,8 @@ enum InterfaceMode: String, CaseIterable, Identifiable {
     case powerUser = "Power User"
 
     var id: InterfaceMode { self }
-
     var localized: LocalizedStringKey { LocalizedStringKey(self.rawValue) }
 
     func index(_ mode: InterfaceMode) -> Int { InterfaceMode.allCases.firstIndex(of: mode)! }
-
     func isAtLeast(_ mode: InterfaceMode) -> Bool { index(self) >= index(mode) }
 }
