663c757705a840743b7f9ca64be1f4885617527d
F1248 <f1248@mailbox.org>
2024-04-07 19:37:13 +0200

    Improve code readability

--- OpenMacHelper/Helpers/SystemProfilerHandler.swift
+++ OpenMacHelper/Helpers/SystemProfilerHandler.swift
@@ -8,5 +8,7 @@
 import Foundation
 
 func getSystemProfiler(_ dataType: String) -> [String: String] {
-    (try! JSONSerialization.jsonObject(with: runProcess(["/usr/sbin/system_profiler", "-json", dataType]).data(using: .utf8)!) as! [String: [[String: String]]])[dataType]![0] // swiftlint:disable:this force_cast force_try force_unwrapping
+    let output: String = runProcess(["/usr/sbin/system_profiler", "-json", dataType])
+    let systemProfiler: [String: [[String: String]]] = try! JSONSerialization.jsonObject(with: output.data(using: .utf8)!) as! [String: [[String: String]]] // swiftlint:disable:this force_try force_unwrapping force_cast
+    return systemProfiler[dataType]![0] // swiftlint:disable:this force_unwrapping
 }
