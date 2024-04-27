b4c1ea2d34f83c2ef104f9e2ffcd5b6aa36a76b0
F1248 <f1248@mailbox.org>
2024-04-27 19:27:32 +0200

    Improve code: extend String with function contains([String])

---
+++ OpenMacHelper/Extensions/String.swift
@@ -0,0 +1,10 @@
+//
+//  String.swift
+//  OpenMacHelper
+//
+//  Created by F1248.
+//
+
+extension String {
+    func contains(_ strings: [String]) -> Bool { strings.contains { contains($0) } }
+}
