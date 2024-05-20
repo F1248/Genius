febc071de3fe15677caf57aa72970a3c43584c78
F1248 <f1248@mailbox.org>
2024-05-20 21:20:20 +0200

    Change icon for Maintenance

--- OpenMacHelper/Views/Sidebar/SidebarView.swift
+++ OpenMacHelper/Views/Sidebar/SidebarView.swift
@@ -18,7 +18,7 @@ struct SidebarView: View {
         List(selection: $selectedSidebarElement) {
             NavigationLinkView(destination: HomeView(), title: "Home", systemImage: "house", tag: .home, keyboardShortcutKey: "0")
             NavigationLinkView(destination: SystemInformationView(), title: interfaceMode.isAtLeast(.simple) ? "System Information" : "Information", systemImage: "info.circle", tag: .systemInformation, keyboardShortcutKey: "1")
-            NavigationLinkView(destination: MaintenanceView(), title: "Maintenance", systemImage: "checkmark.circle", tag: .maintenance, keyboardShortcutKey: "2")
+            NavigationLinkView(destination: MaintenanceView(), title: "Maintenance", systemImage: "gear.badge.checkmark", tag: .maintenance, keyboardShortcutKey: "2")
             NavigationLinkView(destination: SettingsView(), title: "Settings", systemImage: "gear", tag: .settings, keyboardShortcutKey: ",")
         }
         .listStyle(SidebarListStyle())
