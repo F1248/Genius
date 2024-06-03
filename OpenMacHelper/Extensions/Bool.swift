a22d52fff2a59895f80bec80c019af778e0ac2d5
F1248 <f1248@mailbox.org>
2024-06-03 16:55:29 +0200

    Improve parameter naming

--- OpenMacHelper/Extensions/Bool.swift
+++ OpenMacHelper/Extensions/Bool.swift
@@ -7,10 +7,10 @@
 
 extension Bool {
 
-    init?(_ any: Any?) {
-        guard let any = any as? String else { return nil }
-        if any.contains(["enabled", "true", "1", "spfirewall_globalstate_limit_connections", "spfirewall_globalstate_block_all"]) { self.init(true) }
-        if any.contains(["disabled", "false", "0", "spfirewall_globalstate_allow_all"]) { self.init(false) }
+    init?(_ string: Any?) {
+        guard let string = string as? String else { return nil }
+        if string.contains(["enabled", "true", "1", "spfirewall_globalstate_limit_connections", "spfirewall_globalstate_block_all"]) { self.init(true) }
+        if string.contains(["disabled", "false", "0", "spfirewall_globalstate_allow_all"]) { self.init(false) }
         return nil
     }
 }
