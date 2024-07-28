84830c6ba1a9a382a488c49e019a5dd80c544a73
F1248 <f1248@mailbox.org>
2024-07-28 23:19:59 +0200

    Fix function name

--- OpenMacHelper/Extensions/String.swift
+++ OpenMacHelper/Extensions/String.swift
@@ -22,7 +22,7 @@ extension String {
         self = (Mirror(reflecting: localizedStringKey).children.first { $0.label == "key" }?.value as? String ?? "Unknown").localized
     }
 
-    func contains(_ strings: [any StringProtocol]) -> Bool {
+    func contained(_ strings: [any StringProtocol]) -> Bool {
         strings.contains { contains($0) }
     }
 
