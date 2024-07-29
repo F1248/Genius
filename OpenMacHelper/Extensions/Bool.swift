87ffcf1728f3968a882b44144609806a09be0761
F1248 <f1248@mailbox.org>
2024-07-29 11:02:53 +0200

    Improve readability of Bool(Any?)

--- OpenMacHelper/Extensions/Bool.swift
+++ OpenMacHelper/Extensions/Bool.swift
@@ -9,9 +9,24 @@ extension Bool {
 
     init?(_ string: Any?) {
         guard let string = string as? String else { return nil }
-        if string.containsAny(["enabled", "true", "1", "spfirewall_globalstate_limit_connections", "spfirewall_globalstate_block_all"]) { self = true; return }
-        if string.containsAny(["disabled", "false", "0", "spfirewall_globalstate_allow_all"]) { self = false; return }
-        logger.error("Bool(\"\(string)\": Any?) failed: unknown string")
-        return nil
+        if string.containsAny([
+            "enabled",
+            "true",
+            "1",
+            "spfirewall_globalstate_limit_connections",
+            "spfirewall_globalstate_block_all"
+        ]) {
+            self = true
+        } else if string.containsAny([
+            "disabled",
+            "false",
+            "0",
+            "spfirewall_globalstate_allow_all"
+        ]) {
+            self = false
+        } else {
+            logger.error("Bool(\"\(string)\": Any?) failed: unknown string")
+            return nil
+        }
     }
 }
