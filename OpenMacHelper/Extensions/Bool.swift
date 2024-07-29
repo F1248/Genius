59dca417939177afe94278521176d961250eb041
F1248 <f1248@mailbox.org>
2024-07-29 09:50:00 +0200

    Fix function name 2

--- OpenMacHelper/Extensions/Bool.swift
+++ OpenMacHelper/Extensions/Bool.swift
@@ -9,8 +9,8 @@ extension Bool {
 
     init?(_ string: Any?) {
         guard let string = string as? String else { return nil }
-        if string.contains(["enabled", "true", "1", "spfirewall_globalstate_limit_connections", "spfirewall_globalstate_block_all"]) { self = true; return }
-        if string.contains(["disabled", "false", "0", "spfirewall_globalstate_allow_all"]) { self = false; return }
+        if string.containsAny(["enabled", "true", "1", "spfirewall_globalstate_limit_connections", "spfirewall_globalstate_block_all"]) { self = true; return }
+        if string.containsAny(["disabled", "false", "0", "spfirewall_globalstate_allow_all"]) { self = false; return }
         logger.error("Bool(\"\(string)\": Any?) failed: unknown string")
         return nil
     }
