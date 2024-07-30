8fb9317ecb252d171bd926d80ef24607799d2b48
F1248 <f1248@mailbox.org>
2024-07-30 11:12:27 +0200

    Improve order of terms in Bool(Any?)

--- OpenMacHelper/Extensions/Bool.swift
+++ OpenMacHelper/Extensions/Bool.swift
@@ -10,17 +10,17 @@ extension Bool {
     init?(_ string: Any?) {
         guard let string = string as? String else { return nil }
         if string.containsAny([
-            "enabled",
-            "true",
             "1",
+            "true",
+            "enabled",
             "spfirewall_globalstate_limit_connections",
             "spfirewall_globalstate_block_all"
         ]) {
             self = true
         } else if string.containsAny([
-            "disabled",
-            "false",
             "0",
+            "false",
+            "disabled",
             "spfirewall_globalstate_allow_all"
         ]) {
             self = false
