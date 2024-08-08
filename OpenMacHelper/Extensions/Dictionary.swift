85bd5d69192be40a3b368688ea8d89f505357498
F1248 <f1248@mailbox.org>
2024-08-08 20:47:28 +0200

    Remove unused imports

--- OpenMacHelper/Extensions/Dictionary.swift
+++ OpenMacHelper/Extensions/Dictionary.swift
@@ -7,8 +7,6 @@
 //  Created by F1248.
 //
 
-import Foundation
-
 extension Optional {
 
     func contains<Key, Value>(key: Key) -> Bool where Wrapped == [Key: Value] {
--- OpenMacHelper/Types/SystemInformation/SystemInformation.swift
+++ OpenMacHelper/Types/SystemInformation/SystemInformation.swift
@@ -5,7 +5,4 @@
 //  Created by F1248.
 //
 
-import Foundation
-import SwiftUI
-
 enum SystemInformation {}
