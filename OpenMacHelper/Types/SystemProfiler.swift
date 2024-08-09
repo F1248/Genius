2603f9f1a8e01273e871b4058a59883b70019b0a
F1248 <f1248@mailbox.org>
2024-08-09 18:59:41 +0200

    Move SystemProfiler.swift from /Helpers to /Types

---
+++ OpenMacHelper/Types/SystemProfiler.swift
@@ -0,0 +1,21 @@
+//
+//  SystemProfiler.swift
+//  OpenMacHelper
+//
+//  Created by F1248.
+//
+
+import Foundation
+
+enum SystemProfiler {
+
+    nonisolated(unsafe) static let firewall = get("Firewall")
+    nonisolated(unsafe) static let hardware = get("Hardware")
+    nonisolated(unsafe) static let software = get("Software")
+
+    static func get(_ dataType: String) -> [String: Any]? {
+        let dataType: String = "SP\(dataType)DataType"
+        guard let outputData = Data(runProcess(["/usr/sbin/system_profiler", "-json", dataType])) else { return nil }
+        return (try? JSONSerialization.jsonObject(with: outputData) as? [String: [[String: Any]]])?[dataType]?.first
+    }
+}
