02f041bbc15eb5f97b5ee422eb1d274551226e8a
F1248 <f1248@mailbox.org>
2024-08-18 21:52:19 +0200

    Remove unneeded function wrapper

--- OpenMacHelper/Extensions/Foundation/Locale.swift
+++ OpenMacHelper/Extensions/Foundation/Locale.swift
@@ -9,11 +9,10 @@ import Foundation
 
 extension Locale {
 
-    static let currentLanguageCode: String? = {
+    static let currentLanguageCode: String? =
         if #available(macOS 13, *) {
             Locale.current.language.languageCode?.identifier
         } else {
             Locale.current.languageCode
         }
-    }()
 }
