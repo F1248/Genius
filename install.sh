daaec64935123531a5b9e7a00f3de2d0015b8d17
F1248 <f1248@mailbox.org>
2024-08-19 22:25:40 +0200

    Add install shell script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+/bin/echo "
+Preparing..."
+cd /Applications
+
+/bin/echo "Downloading..."
+/usr/bin/curl --silent --location https://nightly.link/F1248/OpenMacHelper/workflows/Build-OpenMacHelper/main/OpenMacHelper.zip --output OpenMacHelper.zip
+
+/bin/echo "Installing..."
+/usr/bin/unzip -q -o OpenMacHelper.zip
+/usr/bin/unzip -q -o OpenMacHelper.zip
+
+/bin/echo "Cleaning up..."
+/bin/rm OpenMacHelper.zip
+
+/bin/echo "Done."
