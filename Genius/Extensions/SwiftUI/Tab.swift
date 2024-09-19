fcfd91f87cf47a15fa6b39d6ca6ad619d5baeab5
F1248 <f1248@mailbox.org>
2024-09-19 17:04:27 +0200

    Replace parameter tableName with variesByInterfaceMode

--- Genius/Extensions/SwiftUI/Tab.swift
+++ Genius/Extensions/SwiftUI/Tab.swift
@@ -11,10 +11,15 @@ import SwiftUICore
 @available(macOS 15, *)
 extension Tab where Value == Never, Content: View, Label == SwiftUI.Label<Text, EmptyView> {
 
-    init(_ titleKey: LocalizedStringKey, tableName: String? = nil, content: () -> Content) {
+    init(
+        _ titleKey: LocalizedStringKey,
+        variesByInterfaceMode: Bool = false,
+        viewInvalidator _: Any? = nil,
+        content: () -> Content
+    ) {
         self.init(content: content) {
             Label {
-                Text(titleKey, tableName: tableName)
+                Text(titleKey, variesByInterfaceMode: variesByInterfaceMode)
             } icon: {
                 EmptyView()
             }
