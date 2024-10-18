8ed5dd2a1a863c2ab264d3c5a61d82f1d5716282
F1248 <f1248@mailbox.org>
2024-10-18 16:27:18 +0200

    Move properties to extension

--- Genius/Models/Settings.swift
+++ Genius/Models/Settings.swift
@@ -6,9 +6,6 @@
 //  See LICENSE.txt for license information.
 //
 
-import SwiftUI
-import SwiftUICore
-
 enum Settings {
 
     enum InterfaceMode: String, CaseIterable, Identifiable, Setting {
@@ -19,20 +16,8 @@ enum Settings {
         case advanced = "Advanced"
         case powerUser = "Power User"
 
-        static let key = "interfaceMode" // swiftlint:disable:this explicit_type_interface
-
         var id: Self { self }
-        var localizedString: String { rawValue.localized() }
-        var localizedStringKey: LocalizedStringKey { LocalizedStringKey(rawValue) }
         var localizationTable: String { "Localizable\(rawValue.remove(" "))" }
-
-        static var value: Self {
-            // swiftformat:disable redundantProperty
-            @AppStorage(key)
-            var value = Self() // swiftlint:disable:this direct_return
-            // swiftformat:enable redundantProperty
-            return value
-        }
 
         init() { self = .normal }
     }
