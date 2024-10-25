d66fef22eae28870a0d04e2e1b1b2f4f4d603518
F1248 <f1248@mailbox.org>
2024-10-25 22:02:28 +0200

    Move initialization of `SystemInformation.Software.OS.bootMode` to variable declaration

--- Genius/Models/SystemInformation/BootMode.swift
+++ Genius/Models/SystemInformation/BootMode.swift
@@ -6,26 +6,9 @@
 // See LICENSE.txt for license information.
 //
 
-import Foundation
-
 enum BootMode {
 
 	case normal
 	case safe
 	case recovery
-
-	init?(_ bootMode: Any?) {
-		if !FileManager.default.fileExists(atPath: "/System/Library/CoreServices/Finder.app") {
-			self = .recovery
-		} else {
-			let bootMode: Self? =
-				switch bootMode as? String {
-				case "normal_boot": .normal
-				case "safe_boot": .safe
-				default: nil
-				}
-			guard let bootMode else { return nil }
-			self = bootMode
-		}
-	}
 }
