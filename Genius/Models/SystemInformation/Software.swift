cefb51868cc0c24b0e6b6d4ad34562e6e34aa75a
F1248 <f1248@mailbox.org>
2024-10-30 20:21:16 +0100

    Split SystemInformation.Hardware.Specifications

--- Genius/Models/SystemInformation/Software.swift
+++ Genius/Models/SystemInformation/Software.swift
@@ -10,6 +10,7 @@ import Foundation
 
 extension SystemInformation {
 
+	// swiftformat:disable organizeDeclarations
 	enum Software {
 
 		enum SMC {
@@ -31,7 +32,6 @@ extension SystemInformation {
 
 		enum OS {
 
-			// swiftformat:disable organizeDeclarations
 			static let components = (SystemProfiler.software?["os_version"] as? String)?.split(separator: " ").map(String.init)
 			static let name: String? = components?[safe: 0]
 			static let version: VersionNumber? = VersionNumber(components?[safe: 1])
@@ -57,7 +57,6 @@ extension SystemInformation {
 				} // swiftformat:disable:next blankLinesBetweenScopes
 			static let bootVolume: String? = SystemProfiler.software?["boot_volume"] as? String
 			static let loaderVersion: VersionNumber? = VersionNumber(SystemProfiler.hardware?["os_loader_version"])
-			// swiftformat:enable organizeDeclarations
 		}
 
 		enum Computer {
