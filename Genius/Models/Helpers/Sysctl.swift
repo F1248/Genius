29254293e661299fef046658412c27df385133f2
F1248 <f1248@mailbox.org>
2024-11-19 20:51:30 +0100

    Move initialization from `Data` from `Sysctl.read(_:)` to extensions

--- Genius/Models/Helpers/Sysctl.swift
+++ Genius/Models/Helpers/Sysctl.swift
@@ -16,12 +16,6 @@ enum Sysctl<W: SysctlValue> {
 		guard sysctlbyname(name, nil, &size, nil, 0) == 0, size > 0 else { return nil }
 		var result = [UInt8](repeating: 0, count: size)
 		guard sysctlbyname(name, &result, &size, nil, 0) == 0 else { return nil }
-		let data = Data(bytes: result, count: result.lastIndex { $0 != 0 }.map(result.index(after:)) ?? size)
-		return switch W.self {
-		case is Bool.Type: (data[0] == 0 ? false : data[0] == 1 ? true : nil) as? W
-		case is Int.Type: data.reversed().reduce(0) { $0 << 8 | Int($1) } as? W
-		case is String.Type: String(data) as? W
-		default: nil
-		}
+		return W(Data(bytes: result, count: result.lastIndex { $0 != 0 }.map(result.index(after:)) ?? size))
 	}
 }
