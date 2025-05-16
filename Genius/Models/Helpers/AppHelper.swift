d18010c07ddf16bd668f86a1a7047e14b9dbd8d7
F1248 <f1248@mailbox.org>
2025-05-16 23:23:05 +0200

    Use shell script for uninstallation, fixes ~/Preferences/dev.F1248.Genius.plist not always being removed

--- Genius/Models/Helpers/AppHelper.swift
+++ Genius/Models/Helpers/AppHelper.swift
@@ -8,26 +8,14 @@
 
 import _Concurrency
 import AppKit
-import Defaults
 import Foundation
 
 enum AppHelper {
 
 	@MainActor
 	static func uninstall() {
-		Defaults.removeAll()
-		if let bundleIdentifier = Bundle.main.bundleIdentifier {
-			let cacheDirectory: URL? = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
-			let libraryDirectory: URL? = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first
-			for url in [
-				cacheDirectory?.appendingPathComponent(bundleIdentifier),
-				libraryDirectory?.appendingPathComponent("HTTPStorages/\(bundleIdentifier)"),
-				libraryDirectory?.appendingPathComponent("Preferences/\(bundleIdentifier).plist"),
-			].compactMap(\.self) {
-				try? FileManager.default.removeItem(at: url)
-			}
-		}
-		try? FileManager.default.removeItem(at: Bundle.main.bundleURL)
+		guard let uninstallPath = Bundle.main.path(forResource: "Uninstall", ofType: nil) else { return }
+		try? Process(uninstallPath)?.run()
 		NSApp.terminate(nil)
 	}
 }
