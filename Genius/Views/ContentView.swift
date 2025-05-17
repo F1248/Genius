727db44c16d546b3f6b837dd93fa53b31d93128c
F1248 <f1248@mailbox.org>
2025-05-17 17:36:17 +0200

    Use non-deprecated `alert`

--- Genius/Views/ContentView.swift
+++ Genius/Views/ContentView.swift
@@ -17,14 +17,18 @@ struct ContentView: View {
 	var body: some View {
 		AdaptingTabView<ContentViewTab>()
 			.frame(minWidth: 686, maxWidth: .infinity, minHeight: 256, maxHeight: .infinity)
-			.alert(isPresented: $observedSharedData.showUninstallAlert) {
-				Alert(
-					title: Text("Are you sure you want to uninstall Genius?".localized),
-					message: Text("Genius and all its data will be uninstalled. This action cannot be undone.".localized),
-					primaryButton: .destructive(Text("Uninstall Genius".localized), action: AppHelper.uninstall),
-					secondaryButton: .cancel(),
-				)
-			}
+			.confirmationDialog(
+				"Are you sure you want to uninstall Genius?".localized,
+				isPresented: $observedSharedData.showUninstallAlert,
+				actions: {
+					Button(role: .destructive, action: AppHelper.uninstall) {
+						VaryingText("Uninstall Genius")
+					}
+				},
+				message: {
+					VaryingText("Genius and all its data will be uninstalled. This action cannot be undone.")
+				},
+			)
 	}
 }
 
