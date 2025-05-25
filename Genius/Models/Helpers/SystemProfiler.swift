07c1f73e4335ffa10ce9f774e8ae7811f7366ad2
F1248 <f1248@mailbox.org>
2025-05-25 20:40:50 +0200

    Simplify `SystemProfiler`

--- Genius/Models/Helpers/SystemProfiler.swift
+++ Genius/Models/Helpers/SystemProfiler.swift
@@ -12,19 +12,21 @@ enum SystemProfiler {
 
 	static let available: Bool? = SystemInformation.Software.OS.bootMode.value !=? .recovery
 
-	static let hardwareValueWrapper = AsyncValueWrapper(valueClosure: { await get("Hardware") })
-	static let softwareValueWrapper = AsyncValueWrapper(valueClosure: { await get("Software") })
+	static let hardwareValueWrapper = get("Hardware")
+	static let softwareValueWrapper = get("Software")
 
 	static var hardware: [String: any Sendable]? { get async { await hardwareValueWrapper.value } }
 	static var software: [String: any Sendable]? { get async { await softwareValueWrapper.value } }
 
-	static func get(_ dataType: String) async -> [String: any Sendable]? {
-		let dataType = "SP\(dataType)DataType" // swiftlint:disable:this explicit_type_interface
-		guard
-			let systemProfilerOutput = await Process("/usr/sbin/system_profiler", "-json", dataType)?.runSafe()
-		else { return nil }
-		return (
-			JSONSerialization.jsonObject(from: systemProfilerOutput) as? [String: [[String: any Sendable]]],
-		)?[dataType]?.first
+	static func get(_ dataType: String) -> AsyncValueWrapper<[String: any Sendable]?> {
+		AsyncValueWrapper(valueClosure: {
+			let dataType = "SP\(dataType)DataType" // swiftlint:disable:this explicit_type_interface
+			guard
+				let systemProfilerOutput = await Process("/usr/sbin/system_profiler", "-json", dataType)?.runSafe()
+			else { return nil }
+			return (
+				JSONSerialization.jsonObject(from: systemProfilerOutput) as? [String: [[String: any Sendable]]],
+			)?[dataType]?.first
+		})
 	}
 }
