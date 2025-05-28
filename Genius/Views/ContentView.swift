b7e8f653ff3585904351b12615cd5a0398e409b7
F1248 <f1248@mailbox.org>
2025-05-29 00:36:43 +0200

    Move uninstall alert to separate file 1/2

--- Genius/Views/ContentView.swift
+++ Genius/Views/ContentView.swift
@@ -9,19 +9,10 @@ import SwiftUICore
 
 struct ContentView: View {
 
-	@ObservedObject var observedSharedData: SharedData = .sharedData
-
 	var body: some View {
 		AdaptingTabView<ContentViewTab>()
 			.frame(minWidth: 686, maxWidth: .infinity, minHeight: 256, maxHeight: .infinity)
-			.alert(isPresented: $observedSharedData.showUninstallAlert) {
-				Alert(
-					title: Text(varying: "Are you sure you want to uninstall Genius?"),
-					message: Text(varying: "Genius and all its data will be uninstalled. This action cannot be undone."),
-					primaryButton: .cancel(),
-					secondaryButton: .destructive(Text(varying: "Uninstall Genius"), action: AppHelper.uninstall),
-				)
-			}
+			.uninstallAlert()
 	}
 }
 
