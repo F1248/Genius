f83c49cc8b26c92e27e18de571760ed47ffeb7ba
F1248 <f1248@mailbox.org>
2025-08-24 23:23:24 +0200

    Fix duplicate extension

--- Genius/Extensions/Foundation/LocalizedStringResource.swift
+++ Genius/Extensions/Foundation/LocalizedStringResource.swift
@@ -3,10 +3,12 @@
 // See LICENSE.txt for license information.
 //
 
+import Defaults
 import Foundation
 
-extension LocalizedStringResource {
+extension LocalizedStringResource: VaryingLocalizationTable {
 
+	static var localizationTable: any VaryingLocalizationTable.Type { Defaults[.interfaceMode].localizationTable }
 	static var supportsInflection: Bool {
 		if #available(macOS 26, *) {
 			true
@@ -15,5 +17,31 @@ extension LocalizedStringResource {
 		}
 	}
 
+	static var administratorPrivilegesRequest: Self { localizationTable.administratorPrivilegesRequest }
+	static var askToAllowAccessoriesToConnect: Self { localizationTable.askToAllowAccessoriesToConnect }
+	static var automaticUpdates: Self { localizationTable.automaticUpdates }
+	static var bootMode: Self { localizationTable.bootMode }
+	static var bootVolume: Self { localizationTable.bootVolume }
+	static var checkForMacOSUpdates: Self { localizationTable.checkForMacOSUpdates }
+	static var cpuFrequency: Self { localizationTable.cpuFrequency }
+	static var cpuName: Self { localizationTable.cpuName }
+	static var cpuType: Self { localizationTable.cpuType }
+	static var downloadMacOSUpdates: Self { localizationTable.downloadMacOSUpdates }
+	static var installAppStoreAppUpdates: Self { localizationTable.installAppStoreAppUpdates }
+	static var installConfigurationDataUpdates: Self { localizationTable.installConfigurationDataUpdates }
+	static var installCriticalUpdates: Self { localizationTable.installCriticalUpdates }
+	static var installMacOSUpdates: Self { localizationTable.installMacOSUpdates }
+	static var malwareProtection: Self { localizationTable.malwareProtection }
+	static var systemInformation: Self { localizationTable.systemInformation }
+	static var uninstallApp: Self { localizationTable.uninstallApp }
+	static var uninstallAppEllipsis: Self { localizationTable.uninstallAppEllipsis }
+	static var uninstallationMessage: Self { localizationTable.uninstallationMessage }
+	static var uninstallationTitle: Self { localizationTable.uninstallationTitle }
+
 	static var user: Self { supportsInflection ? .userWithInflection : .userWithoutInflection }
 }
+
+extension LocalizedStringResource.LocalizableSimple: VaryingLocalizationTable {}
+extension LocalizedStringResource.LocalizableNormal: VaryingLocalizationTable {}
+extension LocalizedStringResource.LocalizableAdvanced: VaryingLocalizationTable {}
+extension LocalizedStringResource.LocalizablePowerUser: VaryingLocalizationTable {}
--- Genius/ViewModels/Localization/VaryingLocalizationTable.swift
+++ Genius/ViewModels/Localization/VaryingLocalizationTable.swift
@@ -3,7 +3,6 @@
 // See LICENSE.txt for license information.
 //
 
-import Defaults
 import Foundation
 
 protocol VaryingLocalizationTable {
@@ -29,34 +28,3 @@ protocol VaryingLocalizationTable {
 	static var uninstallationMessage: LocalizedStringResource { get }
 	static var uninstallationTitle: LocalizedStringResource { get }
 }
-
-extension LocalizedStringResource: VaryingLocalizationTable {
-
-	static var localizationTable: any VaryingLocalizationTable.Type { Defaults[.interfaceMode].localizationTable }
-
-	static var administratorPrivilegesRequest: Self { localizationTable.administratorPrivilegesRequest }
-	static var askToAllowAccessoriesToConnect: Self { localizationTable.askToAllowAccessoriesToConnect }
-	static var automaticUpdates: Self { localizationTable.automaticUpdates }
-	static var bootMode: Self { localizationTable.bootMode }
-	static var bootVolume: Self { localizationTable.bootVolume }
-	static var checkForMacOSUpdates: Self { localizationTable.checkForMacOSUpdates }
-	static var cpuFrequency: Self { localizationTable.cpuFrequency }
-	static var cpuName: Self { localizationTable.cpuName }
-	static var cpuType: Self { localizationTable.cpuType }
-	static var downloadMacOSUpdates: Self { localizationTable.downloadMacOSUpdates }
-	static var installAppStoreAppUpdates: Self { localizationTable.installAppStoreAppUpdates }
-	static var installConfigurationDataUpdates: Self { localizationTable.installConfigurationDataUpdates }
-	static var installCriticalUpdates: Self { localizationTable.installCriticalUpdates }
-	static var installMacOSUpdates: Self { localizationTable.installMacOSUpdates }
-	static var malwareProtection: Self { localizationTable.malwareProtection }
-	static var systemInformation: Self { localizationTable.systemInformation }
-	static var uninstallApp: Self { localizationTable.uninstallApp }
-	static var uninstallAppEllipsis: Self { localizationTable.uninstallAppEllipsis }
-	static var uninstallationMessage: Self { localizationTable.uninstallationMessage }
-	static var uninstallationTitle: Self { localizationTable.uninstallationTitle }
-}
-
-extension LocalizedStringResource.LocalizableSimple: VaryingLocalizationTable {}
-extension LocalizedStringResource.LocalizableNormal: VaryingLocalizationTable {}
-extension LocalizedStringResource.LocalizableAdvanced: VaryingLocalizationTable {}
-extension LocalizedStringResource.LocalizablePowerUser: VaryingLocalizationTable {}
