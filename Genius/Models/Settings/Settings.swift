574d03be4e9640aca70d664ae2cb642f25ebc015
F1248 <f1248@mailbox.org>
2025-10-11 22:51:12 +0200

    Move `Settings.InterfaceMode` to separate file

--- Genius/Models/Settings/Settings.swift
+++ Genius/Models/Settings/Settings.swift
@@ -3,30 +3,4 @@
 // See LICENSE.txt for license information.
 //
 
-import Defaults
-import Foundation
-
-enum Settings {
-
-	enum InterfaceMode: String, CaseIterable, IndexComparable, SelfIdentifiable, Defaults.Serializable {
-
-		case simple = "Simple"
-		case normal = "Normal"
-		case advanced = "Advanced"
-		case powerUser = "PowerUser"
-
-		var title: LocalizedStringResource { switch self {
-			case .simple: .interfaceModeSimple
-			case .normal: .interfaceModeNormal
-			case .advanced: .interfaceModeAdvanced
-			case .powerUser: .interfaceModePowerUser
-		} }
-
-		var localizationTable: any VaryingLocalizationTable.Type { switch self {
-			case .simple: LocalizedStringResource.LocalizableSimple.self
-			case .normal: LocalizedStringResource.LocalizableNormal.self
-			case .advanced: LocalizedStringResource.LocalizableAdvanced.self
-			case .powerUser: LocalizedStringResource.LocalizablePowerUser.self
-		} }
-	}
-}
+enum Settings {}
