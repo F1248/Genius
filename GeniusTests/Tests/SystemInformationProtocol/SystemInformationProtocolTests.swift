4727d6740855b49417962e556a2fb5f9163b6efc
F1248 <f1248@mailbox.org>
2025-11-09 20:36:04 +0100

    Improve grouping of `testCondition` tests

---
+++ GeniusTests/Tests/SystemInformationProtocol/SystemInformationProtocolTests.swift
@@ -0,0 +1,22 @@
+//
+// © 2025 F1248 <f1248@mailbox.org>
+// See LICENSE.txt for license information.
+//
+
+@testable import Genius
+import Testing
+
+@Suite("SystemInformationProtocol")
+struct SystemInformationProtocolTests {
+
+	@Test
+	func testCondition() async { // swiftformat:disable:this swiftTestingTestCaseNames
+		#expect(await SystemInformationData<Int, _>(0 as Int).testCondition == true)
+		#expect(await SystemInformationData<Int?, _>(0, applicable: true).testCondition == true)
+		#expect(await SystemInformationData<Int?, _>(nil, applicable: true).testCondition == false)
+		#expect(await SystemInformationData<Int?, _>(0, applicable: false).testCondition == true)
+		#expect(await SystemInformationData<Int?, _>(nil, applicable: false).testCondition == true)
+		#expect(await SystemInformationData<Int?, _>(0, applicable: nil).testCondition == false)
+		#expect(await SystemInformationData<Int?, _>(nil, applicable: nil).testCondition == false)
+	}
+}
