4a167f0fe0a861a4fdf082ab8531052cfddced7e
F1248 <f1248@mailbox.org>
2026-08-12 20:46:17 +0200

    style(Swift): improve code formatting

--- Genius/Views/SystemInformation/SystemInformationHardwareView.swift
+++ Genius/Views/SystemInformation/SystemInformationHardwareView.swift
@@ -9,33 +9,36 @@ import SwiftUI
 struct SystemInformationHardwareView: View {
 
 	var body: some View {
-		FormView<LocalizedStringResource, _>(textWhenEmpty: .noSystemInformationAvailable, content: [
-			.model: [
-				.name: SystemInformation.Hardware.Model.displayName,
-				.identifier: SystemInformation.Hardware.Model.identifier,
-				.board: SystemInformation.Hardware.Model.board,
-				.number: SystemInformation.Hardware.Model.number,
-				.configurationCode: SystemInformation.Hardware.Model.configCode,
-				.regulatoryNumber: SystemInformation.Hardware.Model.regulatoryNumber,
+		FormView<LocalizedStringResource, _>(
+			textWhenEmpty: .noSystemInformationAvailable,
+			content: [
+				.model: [
+					.name: SystemInformation.Hardware.Model.displayName,
+					.identifier: SystemInformation.Hardware.Model.identifier,
+					.board: SystemInformation.Hardware.Model.board,
+					.number: SystemInformation.Hardware.Model.number,
+					.configurationCode: SystemInformation.Hardware.Model.configCode,
+					.regulatoryNumber: SystemInformation.Hardware.Model.regulatoryNumber,
+				],
+				.specifications: [
+					.securityChip: SystemInformation.Hardware.securityChip,
+					.cpuType: SystemInformation.Hardware.CPU.type,
+					.cpuName: SystemInformation.Hardware.CPU.name,
+					.cpuIdentifier: SystemInformation.Hardware.CPU.identifier,
+					.cpuFrequency: SystemInformation.Hardware.CPU.frequency,
+					(SystemInformation.Hardware.CPU.Cores.differentTypes ?? true) ? .totalCores : .cores:
+						SystemInformation.Hardware.CPU.Cores.total,
+					.performanceCores: SystemInformation.Hardware.CPU.Cores.performance,
+					.efficiencyCores: SystemInformation.Hardware.CPU.Cores.efficiency,
+					.memory: SystemInformation.Hardware.memory,
+				],
+				.machine: [
+					.serialNumber: SystemInformation.Hardware.Machine.serialNumber,
+					.hardwareUUID: SystemInformation.Hardware.Machine.hardwareUUID,
+					.provisioningUDID: SystemInformation.Hardware.Machine.provisioningUDID,
+				],
 			],
-			.specifications: [
-				.securityChip: SystemInformation.Hardware.securityChip,
-				.cpuType: SystemInformation.Hardware.CPU.type,
-				.cpuName: SystemInformation.Hardware.CPU.name,
-				.cpuIdentifier: SystemInformation.Hardware.CPU.identifier,
-				.cpuFrequency: SystemInformation.Hardware.CPU.frequency,
-				(SystemInformation.Hardware.CPU.Cores.differentTypes ?? true) ? .totalCores : .cores:
-					SystemInformation.Hardware.CPU.Cores.total,
-				.performanceCores: SystemInformation.Hardware.CPU.Cores.performance,
-				.efficiencyCores: SystemInformation.Hardware.CPU.Cores.efficiency,
-				.memory: SystemInformation.Hardware.memory,
-			],
-			.machine: [
-				.serialNumber: SystemInformation.Hardware.Machine.serialNumber,
-				.hardwareUUID: SystemInformation.Hardware.Machine.hardwareUUID,
-				.provisioningUDID: SystemInformation.Hardware.Machine.provisioningUDID,
-			],
-		])
+		)
 	}
 }
 
--- Genius/Views/SystemInformation/SystemInformationSoftwareView.swift
+++ Genius/Views/SystemInformation/SystemInformationSoftwareView.swift
@@ -9,34 +9,37 @@ import SwiftUI
 struct SystemInformationSoftwareView: View {
 
 	var body: some View {
-		FormView<LocalizedStringResource, _>(textWhenEmpty: .noSystemInformationAvailable, content: [
-			.smc: [
-				.version: SystemInformation.Software.SMC.version,
+		FormView<LocalizedStringResource, _>(
+			textWhenEmpty: .noSystemInformationAvailable,
+			content: [
+				.smc: [
+					.version: SystemInformation.Software.SMC.version,
+				],
+				.firmware: [
+					.version: SystemInformation.Software.Firmware.version,
+				],
+				.kernel: [
+					.name: SystemInformation.Software.Kernel.name,
+					.version: SystemInformation.Software.Kernel.version,
+				],
+				.macOS: [
+					.version: SystemInformation.Software.OS.version,
+					.codeName: SystemInformation.Software.OS.codeName,
+					.buildNumber: SystemInformation.Software.OS.buildNumber,
+					.bootMode: SystemInformation.Software.OS.bootMode,
+					.bootVolume: SystemInformation.Software.OS.bootVolume,
+					.loaderVersion: SystemInformation.Software.OS.loaderVersion,
+				],
+				.computer: [
+					.name: SystemInformation.Software.Computer.name,
+					.hostname: SystemInformation.Software.Computer.hostName,
+				],
+				.user: [
+					.name: SystemInformation.Software.User.name,
+					.accountName: SystemInformation.Software.User.accountName,
+				],
 			],
-			.firmware: [
-				.version: SystemInformation.Software.Firmware.version,
-			],
-			.kernel: [
-				.name: SystemInformation.Software.Kernel.name,
-				.version: SystemInformation.Software.Kernel.version,
-			],
-			.macOS: [
-				.version: SystemInformation.Software.OS.version,
-				.codeName: SystemInformation.Software.OS.codeName,
-				.buildNumber: SystemInformation.Software.OS.buildNumber,
-				.bootMode: SystemInformation.Software.OS.bootMode,
-				.bootVolume: SystemInformation.Software.OS.bootVolume,
-				.loaderVersion: SystemInformation.Software.OS.loaderVersion,
-			],
-			.computer: [
-				.name: SystemInformation.Software.Computer.name,
-				.hostname: SystemInformation.Software.Computer.hostName,
-			],
-			.user: [
-				.name: SystemInformation.Software.User.name,
-				.accountName: SystemInformation.Software.User.accountName,
-			],
-		])
+		)
 	}
 }
 
